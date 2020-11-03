0f22b6eba30ba6bfecf2f6ef4f9e149d055f794e
Rajitha Opanayaka <rajithaopanayaka.ro@gmail.com>
2020-11-03 14:29:34 +0530

    client implement using thread

--- client.py
+++ client.py
@@ -3,41 +3,61 @@ import json
 import socket
 import numpy as np
 import struct
+from threading import *
 
-payload_size = struct.calcsize("L")  ### CHANGED
-data = b''
-def send(c,data):
-	data_string=pickle.dumps(data)
-	message_size = struct.pack("L", len(data_string))
-	c.sendall(message_size+data_string)	
-def receive_array(data,payload_size,conn):
-        while len(data) < payload_size:
-            data += conn.recv(4096)
+class client(Thread):
+	payload_size = struct.calcsize("L")  ### CHANGED
+	data = b''
+	valdict=0
+	x=0
+	def __init__(self,values):
+		super(client, self).__init__()
+		self.valdict=values
+	
+	def send(self,c,data):
+		data_string=pickle.dumps(data)
+		message_size = struct.pack("L", len(data_string))
+		c.sendall(message_size+data_string)	
+	def receive_array(self,data,payload_size,conn):
+		while len(data) < payload_size:
+			data += conn.recv(4096)
 
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("L", packed_msg_size)[0]
+		packed_msg_size = data[:payload_size]
+		data = data[payload_size:]
+		msg_size = struct.unpack("L", packed_msg_size)[0]
 
         # Retrieve all data based on message size
-        while len(data) < msg_size:
-            data += conn.recv(4096)
+		while len(data) < msg_size:
+			data += conn.recv(4096)
 
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
+		frame_data = data[:msg_size]
+		data = data[msg_size:]
 
         # Extract frame
-        frame = pickle.loads(frame_data)
-        return frame
+		frame = pickle.loads(frame_data)
+		return frame
+		
+	def run(self):
+		c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		c.connect(('localhost',9999)) #ip address and port
+		self.send(c,self.valdict)
+		self.x=self.receive_array(self.data,self.payload_size,c)
+	def value(self):
+		return self.x
 
 
+#c.connect(('localhost',9999)) #ip address and port
+#send data to server
+#send(c,conv_dict)
+#receive data from server
+#out=receive_array(data,payload_size,c)
+#print(out["data"].shape)
 np.random.seed(1)
 image=np.random.randn(1,256,256, 3) #h256X256 image
 conv_dict = { "data":image}
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-c.connect(('localhost',9999)) #ip address and port
-#send data to server
-send(c,conv_dict)
-#receive data from server
-out=receive_array(data,payload_size,c)
-print(out["data"].shape)
+c=client(conv_dict)
+c.start()
+print("Byee")
+c.join()
+print(c.value()["data"].shape)
+print("Byee")
